f9724a64a69c2e8722e00552b67a1cb9ef729b99
Aidan <yidong.guan@gmail.com>
2014-03-28 17:38:12 +0800

    rails new

---
+++ bookingsystem/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookingsystem::Application.config.secret_key_base = 'c2002899e4211c276cec67acaea2771b700eaf43c1a0574a438fb57c4339b0dc4cecb11ec2173b0620b02d99eff566652e5b1fd40ead887f778ace23b26f1589'
